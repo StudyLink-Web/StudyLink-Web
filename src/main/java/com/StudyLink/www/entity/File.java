e0d72a488bea7ed96a76b60d748337e4d4b2fa70
rlarhkdwn <rlarhkdwn0524@gmail.com>
2026-01-08 11:31:09 +0900

    chore: FileHandler 작성

---
+++ src/main/java/com/StudyLink/www/entity/File.java
@@ -0,0 +1,40 @@
+package com.StudyLink.www.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class File{
+    @Id
+    private String uuid;
+
+    @Column(name = "save_dir", nullable = false)
+    private String saveDir;
+
+    @Column(name = "file_name", nullable = false)
+    private String fileName;
+
+    @Column(name = "file_type", nullable = false, columnDefinition = "int default 0")
+    private int fileType;
+
+    private long bno;
+
+    @Column(name = "file_size")
+    private long fileSize;
+
+    @Column
+    private LocalDateTime regDate;
+
+    @Column
+    private LocalDateTime modDate;
+}
