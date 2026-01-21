ca8e4f74ec5cbfba06f483a9ba9cb301a4435372
shinjw3610 <shinjw3610@gmail.com>
2026-01-21 17:04:10 +0900

    불필요 파일 삭제

--- src/main/java/com/StudyLink/www/repository/CommunityPostRepository.java
+++
@@ -1,7 +0,0 @@
-package com.StudyLink.www.repository;
-
-import com.StudyLink.www.entity.CommunityPost;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommunityPostRepository extends JpaRepository<CommunityPost, Long> {
-}
