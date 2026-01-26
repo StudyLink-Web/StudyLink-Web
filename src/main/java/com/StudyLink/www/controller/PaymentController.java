da96f0cccc6177ece8831aa295f7967af72e9330
rlarhkdwn <rlarhkdwn0524@gmail.com>
2026-01-26 09:26:42 +0900

    chore: 환불 요청 오류 처리

--- src/main/java/com/StudyLink/www/controller/PaymentController.java
+++ src/main/java/com/StudyLink/www/controller/PaymentController.java
@@ -135,7 +135,6 @@ public class PaymentController {
 
         int isOk = paymentService.insertExchangeRequest(request, userId);
 
-
-        return ResponseEntity.ok("OK");
+        return isOk == 1 ? ResponseEntity.ok("OK") : ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("서버 오류가 발생했습니다. 잠시 후 다시 시도해주세요.");
     }
 }
